af042d5d4d0944de08689d3c7c12946ff02b0bba
Joe Hermaszewski <git@monoid.al>
2016-03-06 00:01:09 +0000

    All new accessors

--- src/Data/Vector/Generic/Sized.hs
+++ src/Data/Vector/Generic/Sized.hs
@@ -8,6 +8,35 @@
 
 module Data.Vector.Generic.Sized
  ( Vector
+   -- * Accessors
+   -- ** Length information 
+  , length
+  , length'
+    -- ** Indexing
+  , index
+  , index'
+  , unsafeIndex
+  , head
+  , last
+    -- ** Monadic indexing
+  , indexM
+  , indexM'
+  , unsafeIndexM
+  , headM
+  , lastM
+    -- ** Extracting subvectors (slicing)
+  , slice
+  , slice'
+  , init
+  , tail
+  , take
+  , take'
+  , drop
+  , drop'
+  , splitAt
+  , splitAt'
+
+
     -- * Construction
   , fromVector
   , replicate
@@ -15,16 +44,6 @@ module Data.Vector.Generic.Sized
   , generate
     -- * Monadic Construction
   , generateM
-    -- * Elimination
-  , length
-  , index
-  , head
-  , last
-    -- * Extract subsets
-  , tail
-  , init
-  , take
-  , drop
     -- * Mapping
   , map
     -- * Monadic Mapping
@@ -41,8 +60,8 @@ import Data.Proxy
 import Control.DeepSeq
 import Foreign.Storable
 import Foreign.Ptr (castPtr)
-import Prelude hiding (replicate, head, last,
-                       tail, init, map, length, drop, take)
+import Prelude hiding (replicate, head, last, tail, init, map, length, drop,
+                       take, splitAt)
 
 newtype Vector v (n :: Nat) a = Vector (v a)
   deriving (Show, Eq, Ord, Foldable, NFData)
@@ -53,6 +72,171 @@ instance (KnownNat n, Storable a)
   alignment _ = alignment (undefined :: a)
   peek ptr = generateM (Proxy :: Proxy n) (peekElemOff (castPtr ptr))
   poke ptr = imapM_ (pokeElemOff (castPtr ptr))
+
+-- | /O(1)/ Yield the length of the vector as an 'Int'.
+length :: forall v n a. (KnownNat n)
+       => Vector v n a -> Int
+length _ = fromInteger (natVal (Proxy :: Proxy n))
+{-# inline length #-}
+
+-- | /O(1)/ Yield the length of the vector as a 'Proxy'.
+length' :: forall v n a. (KnownNat n)
+        => Vector v n a -> Proxy n
+length' _ = Proxy
+{-# inline length' #-}
+
+-- | /O(1)/ Indexing using an Int.
+index :: forall v n a. (KnownNat n, VG.Vector v a)
+      => Vector v n a -> Int -> a
+index (Vector v) i = v VG.! i
+{-# inline index #-}
+
+-- | /O(1)/ Safe indexing using a 'Proxy'.
+index' :: forall v n m a. (KnownNat n, KnownNat m, VG.Vector v a)
+       => Vector v (n+m) a -> Proxy n -> a
+index' (Vector v) p = v `VG.unsafeIndex` i
+  where i = fromInteger (natVal p)
+{-# inline index' #-}
+
+-- | /O(1)/ Indexing using an Int without bounds checking.
+unsafeIndex :: forall v n a. (KnownNat n, VG.Vector v a)
+      => Vector v n a -> Int -> a
+unsafeIndex (Vector v) i = v `VG.unsafeIndex` i
+{-# inline unsafeIndex #-}
+
+-- | /O(1)/ Yield the first element of a non-empty vector.
+head :: forall v n a. (VG.Vector v a)
+     => Vector v (n+1) a -> a
+head (Vector v) = VG.unsafeHead v
+{-# inline head #-}
+
+-- | /O(1)/ Yield the last element of a non-empty vector.
+last :: forall v n a. (VG.Vector v a)
+     => Vector v (n+1) a -> a
+last (Vector v) = VG.unsafeLast v
+{-# inline last #-}
+
+-- | /O(1)/ Indexing in a monad. See the documentation for 'VG.indexM' for an
+-- explanation of why this is useful.
+indexM :: forall v n a m. (KnownNat n, VG.Vector v a, Monad m)
+      => Vector v n a -> Int -> m a
+indexM (Vector v) i = v `VG.indexM` i
+{-# inline indexM #-}
+
+-- | /O(1)/ Safe indexing in a monad using a 'Proxy'. See the documentation for
+-- 'VG.indexM' for an explanation of why this is useful.
+indexM' :: forall v n k a m. (KnownNat n, KnownNat k, VG.Vector v a, Monad m)
+      => Vector v (n+k) a -> Proxy n -> m a
+indexM' (Vector v) p = v `VG.indexM` i
+  where i = fromInteger (natVal p)
+{-# inline indexM' #-}
+
+-- | /O(1)/ Indexing using an Int without bounds checking. See the
+-- documentation for 'VG.indexM' for an explanation of why this is useful.
+unsafeIndexM :: forall v n a m. (KnownNat n, VG.Vector v a, Monad m)
+      => Vector v n a -> Int -> m a
+unsafeIndexM (Vector v) i = v `VG.unsafeIndexM` i
+{-# inline unsafeIndexM #-}
+
+-- | /O(1)/ Yield the first element of a non-empty vector in a monad. See the
+-- documentation for 'VG.indexM' for an explanation of why this is useful.
+headM :: forall v n a m. (KnownNat n, VG.Vector v a, Monad m)
+      => Vector v (n+1) a -> m a
+headM (Vector v) = VG.unsafeHeadM v
+{-# inline headM #-}
+
+-- | /O(1)/ Yield the last element of a non-empty vector in a monad. See the
+-- documentation for 'VG.indexM' for an explanation of why this is useful.
+lastM :: forall v n a m. (KnownNat n, VG.Vector v a, Monad m)
+      => Vector v (n+1) a -> m a
+lastM (Vector v) = VG.unsafeLastM v
+{-# inline lastM #-}
+
+-- | /O(1)/ Yield a slice of the vector without copying it with an inferred
+-- length argument.
+slice :: forall v i n a. (KnownNat i, KnownNat n, VG.Vector v a)
+      => Proxy i -- ^ starting index
+      -> Vector v (i+n) a
+      -> Vector v n a
+slice pi (Vector v) = Vector (VG.unsafeSlice i n v)
+  where i = fromInteger (natVal pi)
+        n = fromInteger (natVal (Proxy :: Proxy n))
+{-# inline slice #-}
+
+-- | /O(1)/ Yield a slice of the vector without copying it with an explicit
+-- length argument.
+slice' :: forall v i n a. (KnownNat i, KnownNat n, VG.Vector v a)
+       => Proxy i -- ^ starting index
+       -> Proxy n -- ^ length
+       -> Vector v (i+n) a
+       -> Vector v n a
+slice' pi _ = slice pi
+{-# inline slice' #-}
+
+-- | /O(1)/ Yield all but the last element of a non-empty vector without
+-- copying.
+init :: forall v n a. (VG.Vector v a)
+     => Vector v (n+1) a -> Vector v n a
+init (Vector v) = Vector (VG.unsafeInit v)
+{-# inline init #-}
+
+-- | /O(1)/ Yield all but the first element of a non-empty vector without
+-- copying.
+tail :: forall v n a. (VG.Vector v a)
+     => Vector v (n+1) a -> Vector v n a
+tail (Vector v) = Vector (VG.unsafeTail v)
+{-# inline tail #-}
+
+-- | /O(1)/ Yield the first n elements. The resultant vector always contains
+-- this many elements. The length of the resultant vector is inferred from the
+-- type.
+take :: forall v n m a. (KnownNat n, KnownNat m, VG.Vector v a)
+     => Vector v (m+n) a -> Vector v n a
+take (Vector v) = Vector (VG.unsafeTake i v)
+  where i = fromInteger (natVal (Proxy :: Proxy n))
+{-# inline take #-}
+
+-- | /O(1)/ Yield the first n elements. The resultant vector always contains
+-- this many elements. The length of the resultant vector is given explicitly
+-- as a 'Proxy' argument.
+take' :: forall v n m a. (KnownNat n, KnownNat m, VG.Vector v a)
+      => Proxy n -> Vector v (m+n) a -> Vector v n a
+take' _ = take
+{-# inline take' #-}
+
+-- | /O(1)/ Yield all but the the first n elements. The given vector must
+-- contain at least this many elements The length of the resultant vector is
+-- inferred from the type.
+drop :: forall v n m a. (KnownNat n, KnownNat m, VG.Vector v a)
+     => Vector v (m+n) a -> Vector v m a
+drop (Vector v) = Vector (VG.unsafeDrop i v)
+  where i = fromInteger (natVal (Proxy :: Proxy n))
+{-# inline drop #-}
+
+-- | /O(1)/ Yield all but the the first n elements. The given vector must
+-- contain at least this many elements The length of the resultant vector is
+-- givel explicitly as a 'Proxy' argument.
+drop' :: forall v n m a. (KnownNat n, KnownNat m, VG.Vector v a)
+      => Proxy n -> Vector v (m+n) a -> Vector v m a
+drop' _ = drop
+{-# inline drop' #-}
+
+-- | /O(1)/ Yield the first n elements paired with the remainder without copying. 
+-- The lengths of the resultant vector are inferred from the type.
+splitAt :: forall v n m a. (KnownNat n, KnownNat m, VG.Vector v a)
+        => Vector v (n+m) a -> (Vector v n a, Vector v m a)
+splitAt (Vector v) = (Vector a, Vector b)
+  where i = fromInteger (natVal (Proxy :: Proxy n))
+        (a, b) = VG.splitAt i v
+{-# inline splitAt #-}
+
+-- | /O(1)/ Yield the first n elements paired with the remainder without
+-- copying.  The length of the first resultant vector is passed explicitly as a
+-- 'Proxy' argument.
+splitAt' :: forall v n m a. (KnownNat n, KnownNat m, VG.Vector v a)
+         => Proxy n -> Vector v (n+m) a -> (Vector v n a, Vector v m a)
+splitAt' _ = splitAt
+{-# inline splitAt' #-}
 
 -- | Convert a 'Data.Vector.Generic.Vector' into a
 -- 'Data.Vector.Generic.Sized.Vector' if it has the correct size, otherwise
@@ -91,58 +275,6 @@ withVectorUnsafe :: forall a b v (n :: Nat). (VG.Vector v a, VG.Vector v b)
                  => (v a -> v b) -> Vector v n a -> Vector v n b
 withVectorUnsafe f (Vector v) = Vector (f v)
 {-# INLINE withVectorUnsafe #-}
-
--- | /O(1)/ Index safely into the vector using a type level index.
-index :: forall (m :: Nat) a v (n :: Nat). (KnownNat n, KnownNat m, VG.Vector v a)
-      => Vector v (m+n) a -> Proxy n -> a
-index (Vector v) i = v `VG.unsafeIndex` fromIntegral (natVal i)
-{-# INLINE index #-}
-
--- | /O(1)/ Yield the first n elements. The resultant vector always contains
--- this many elements.
-take :: forall (m :: Nat) a v (n :: Nat). (KnownNat n, KnownNat m, VG.Vector v a)
-     => Proxy n -> Vector v (m+n) a -> Vector v n a
-take i (Vector v) = Vector (VG.take (fromIntegral $ natVal i) v)
-{-# INLINE take #-}
-
--- | /O(1)/ Yield all but the first n elements.
-drop :: forall (m :: Nat) a v (n :: Nat). (KnownNat n, KnownNat m, VG.Vector v a)
-     => Proxy n -> Vector v (m+n) a -> Vector v m a
-drop i (Vector v) = Vector (VG.drop (fromIntegral $ natVal i) v)
-{-# INLINE drop #-}
-
--- | /O(1)/ Get the length of the vector.
-length :: forall a v (n :: Nat). (VG.Vector v a)
-       => Vector v n a -> Int
-length (Vector v) = VG.length v
-{-# INLINE length #-}
-
--- | /O(1)/ Get the first element of a non-empty vector.
-head :: forall a v (n :: Nat). (VG.Vector v a)
-     => Vector v (n+1) a -> a
-head (Vector v) = VG.head v
-{-# INLINE head #-}
-
--- | /O(1)/ Get the last element of a non-empty vector.
-last :: forall a v (n :: Nat). (VG.Vector v a)
-     => Vector v (n+1) a -> a
-last (Vector v) = VG.last v
-{-# INLINE last #-}
-
--- | /O(1)/ Yield all but the first element of a non-empty vector without
--- copying.
-tail :: forall a v (n :: Nat). (VG.Vector v a)
-     => Vector v (n+1) a -> Vector v n a
-tail (Vector v) = Vector (VG.tail v)
-{-# INLINE tail #-}
-
--- | /O(1)/ Yield all but the last element of a non-empty vector without
--- copying.
-init :: forall a v (n :: Nat). (VG.Vector v a)
-     => Vector v (n+1) a -> Vector v n a
-init (Vector v) = Vector (VG.init v)
-{-# INLINE init #-}
-
 -- | /O(n)/ Construct a vector with the same element in each position.
 replicate :: forall a v (n :: Nat). (VG.Vector v a, KnownNat n)
           => a -> Vector v n a
