6183d6b663a437b0f0687d1dc212b1eae9eed333
Justin Le <justin@jle.im>
2018-01-25 20:58:44 -0800

    Storable and NFData instances

--- src/Data/Vector/Generic/Mutable/Sized/Internal.hs
+++ src/Data/Vector/Generic/Mutable/Sized/Internal.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE KindSignatures #-}
 
 module Data.Vector.Generic.Mutable.Sized.Internal (
@@ -9,9 +10,11 @@ module Data.Vector.Generic.Mutable.Sized.Internal (
 
 import GHC.Generics (Generic)
 import GHC.TypeLits
+import Control.DeepSeq (NFData)
 import Data.Data
+import Foreign.Storable
 
 -- | A wrapper to tag mutable vectors with a type level length.
 newtype MVector v (n :: Nat) s a = MVector (v s a)
-  deriving ( Generic, Typeable, Data )
+  deriving ( Generic, Typeable, Data, Storable, NFData )
 
