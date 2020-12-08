82172f844a729ee90ca051bc3001f489cf8a7321
Joe Hermaszewski <git@monoid.al>
2020-12-08 13:02:56 +0800

    Add release.nix to generate tarballs for Hackage

---
+++ release.nix
@@ -0,0 +1,10 @@
+{ pkgs ? import <nixpkgs> { }, compiler ? null }:
+
+with pkgs.haskell.lib;
+
+let drv = import ./default.nix { inherit pkgs compiler; };
+in {
+  tarball = sdistTarball drv;
+  docs = documentationTarball drv;
+  sdistTest = buildFromSdist drv;
+}
